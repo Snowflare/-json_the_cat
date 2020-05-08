f14d902d0cde79243ebbe6f3e6ad49934bd146ba
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-08 00:16:50 +0000

    first commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,36 @@
 const request = require('request');
-const arg = process.argv.slice(2);
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-     
-  if (body === '[]') {
-    console.log('Breed Not Found');
-  } else if (error !== null) {
-    console.log(error);
-    
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error){
+      callback(error, null);
+      return;
+    }
     const data = JSON.parse(body);
-    console.log('statusCode:', response && response.statusCode);
-    console.log(data[0].description);
-  }
-  
-});
+    if (!Array.isArray(data)){
+      if (typeof data === 'object') {
+        if (data.status === 404) {
+          callback('Breed url not found', null);
+          return;
+        }else {
+          callback('unknown error:' + body, null);
+          return;
+        }
+      }else {
+        callback('Total unknown error', null);
+        return;
+      }
+      
+    }
+    if (data.length === 0) {
+      callback('Breed not found', null);
+      return;
+    }
+    // console.log(body);
+    // console.log(data);
+    
+    
+    callback(null, data[0].description);
+  });
+};
+
+module.exports.fetchBreedDescription = fetchBreedDescription;
